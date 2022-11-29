706b00f5ca235eab27baf14060edfd1a98182e80
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-28 19:12:53 -0500

    commit tree 87775c92284a4fb1ec8f40df0ff3667d29c79492

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+
+
+  resources :users, only: [:show, :index] do 
+    resources :items, only: [:index, :show, :create]
+  end
+
+  resources :items, only: [:index, :show, :create]
+
 end
